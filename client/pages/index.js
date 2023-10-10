34055f11e6c78046d82f494f74a44cb3e75f7167
James Coleman <colema18@gmail.com>
2023-10-10 12:46:42 -0700

    header and global nav in place

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,7 +12,7 @@ LandingPage.getInitialProps = async (context) => {
     const client = buildClient(context);
     const { data } = await client.get('/api/users/currentuser');
 
-  return data;
+    return data;
 };
 
 export default LandingPage;
