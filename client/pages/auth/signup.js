e9bd1c99b48d855f94d87d1628666f1997908c02
James Coleman <colema18@gmail.com>
2023-10-10 10:18:42 -0700

    initial routing for getInitialProps

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,23 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 const Signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email,
+            password
+        },
+        onSuccess: () => Router.push('/')
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
-
-        try{
-            const response = await axios.post('/api/users/signup', { email, password });
-
-            console.log(response.data);
-        }
-        catch(err){
-            setErrors(err.response.data.errors);
-        }
-
+        doRequest();
     };
 
     return (
@@ -43,16 +43,7 @@ const Signup = () => {
                     id="password" 
                     placeholder="Password" />
             </div>
-            { errors.length > 0 &&
-                <div className="alert alert-danger">
-                    <h4>Oops....</h4>
-                    <ul>
-                        {errors.map(err => (
-                            <li key={err.message}>{err.message}</li>
-                        ))}
-                    </ul>
-                </div>
-            }
+            {errors}
             <button type="submit" className="btn btn-primary">Sign Up</button>
         </form>
     );
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,33 @@
-const Landing = () => {
-    return <h1>Landing Page</h1>;
-};
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
+    // axios.get('/api/users/currentuser').catch((err) => {
+    //   console.log(err.message);
+    // });
    
-export default Landing;
+    return <h1>Landing Page</h1>;
+  };
+
+LandingPage.getInitialProps = async () => {
+    if(typeof window === undefined){
+        console.log('calling from server');
+        const {data} = await axios.get(
+            'https://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: {
+                    host: 'ticketing.dev'
+                }
+            }
+        );
+        return {currentUser: data};
+        return data;
+    }else{
+        console.log('calling from client');
+        const {data} = await axios.get('/api/users/currentuser');
+        return data;
+    }
+};
+
+
+export default LandingPage;
