ab7390a80007b7f54c9811ed1b8c934a7b3e1cae
James Coleman <james@koxa.io>
2023-11-01 12:02:28 -0700

    tickets service in place an start of NATS streaming

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket'; 
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
     const response = await request(app)
@@ -64,14 +65,21 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
-    //add in a check to ensure the ticket was saved in db
-    
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
+    const title = 'title of ticket';
+    const price = 10;
     await request(app)
         .post('/api/tickets')
         .set('Cookie', global.signin())
         .send({
-            title: 'title of ticket',
-            price: 10
+            title: title,
+            price: price
         })
         .expect(201);
+    
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].price).toEqual(price);
+    expect(tickets[0].title).toEqual(title);
 });
